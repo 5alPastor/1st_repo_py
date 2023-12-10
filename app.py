ccfede75712c159494ba2deb4dd6cf7c8db2a577
5alPastor <127814978+5alPastor@users.noreply.github.com>
2023-12-09 22:41:59 -0600

    initial commit

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+$ ls -a
+
+.git .gitignore .streamlit app.py README.md requirements.txt
+
+st.header('Lanzar una moneda')
+
+st.write('esta aplicación aún no es funcional. En construcción')
